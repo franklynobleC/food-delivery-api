0289940a5506776873b1e584d5ad6808ad7c830d
franklynobleC <franklyessien01@gmail.com>
2023-10-07 10:12:30 +0100

    Orders Model with all values

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,53 @@
-const  mongoose =  require('mongoose')
+const mongoose = require('mongoose')
+
+const SingleOrderItemSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  },
+  amount: {
+    type: String,
+
+    required: [true, 'amount can  not  be Empty please provide amount']
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'quantity can  not  be Empty please provide quantity']
+  },
+  food: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Food',
+    required: true
+  }
+})
+const OrderSchema = new mongoose.Schema(
+  {
+    OrderItems: [SingleOrderItemSchema],
+    total: {
+      type: Number,
+      required: true
+    },
+    status: {
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending'
+    },
+
+    deliveryFee: {
+      type: Number,
+      required: [true, 'please provide Delivery fee, make sure type is number'],
+      default: 200
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Order', OrderSchema)
