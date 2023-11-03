28b4d650351d9b9444b003eec3059ffffac6489b
franklynobleC <franklyessien01@gmail.com>
2023-11-03 22:32:39 +0100

    login  page  test

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -44,10 +44,7 @@ function Auth () {
           <div>
             <label className='font-medium'>Email</label>
             <input
-              type='text'
-              name='email'
-              value={email1}
-              onChange={e => setUserEmail(e.target.value)}
+              type='email'
               required
               className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg'
             />
@@ -55,10 +52,8 @@ function Auth () {
           <div>
             <label className='font-medium'>Password</label>
             <input
-              type='text'
-              name='password'
-              value={password1}
-              onChange={e => setUserPassword(e.target.value)}
+              type='password'
+              required
               className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg'
             />
           </div>
