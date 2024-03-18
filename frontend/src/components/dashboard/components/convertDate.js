1b80a28dab8fcf82441aa79157727b2da4a24efe
franklynobleC <franklyessien01@gmail.com>
2024-03-18 19:49:57 +0100

    seperate frontend   code

--- frontend/src/components/dashboard/components/convertDate.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import moment from 'moment'
-
-export const convertDate = dateData => {
-
-console.log(dateData)
-  return moment(dateData).format('DD/MM/YYYY, h:mm:ss a')
-}
